6f81fcb09826c10589a0fecb7c9b739c744121c0
Herr Fisk <mac@noreply.com>
2015-09-11 00:46:45 +0200

    Sketch of index.html and scss loader for webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 module.exports = {
   context: __dirname,
+
   entry: "./src/main.js", //the loader
+
   output: {
-    //path: "./build/", //build to this dir
+    path: "./build/", //build to this dir
+    filename: "banan.js", //name that the bundle will have
     publicPath: "/assets/", //public endpoint
-    filename: "./banan.js", //name that the bundle will have
   },
+
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }
+    ]
+  },
+
   devServer: {
-    //contentBase: "./build/", //serve from this folder
+    //contentBase: "./build/", //serve from this folder, but will not find index.html then.
     //noInfo: true, //  --no-info option
     //progress: true,
     // proxy: {
