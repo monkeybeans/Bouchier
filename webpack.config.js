73bff5cc64d1ee0cfe2e75a2b28831c8af3910a1
Herr Fisk <mac@noreply.com>
2015-09-11 00:11:37 +0200

    Webpack dev server woring okay.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  entry: {
-    app: ["./src/main.js"], //the loader
-  },
+  context: __dirname,
+  entry: "./src/main.js", //the loader
   output: {
-    path: "./build", //build to this dir
-    publicPath: "/assets/", //serve from this
-    filename: "banan.js", //name that the bundle will have
+    //path: "./build/", //build to this dir
+    publicPath: "/assets/", //public endpoint
+    filename: "./banan.js", //name that the bundle will have
+  },
+  devServer: {
+    //contentBase: "./build/", //serve from this folder
+    //noInfo: true, //  --no-info option
+    //progress: true,
+    // proxy: {
+    //     "*": "http://localhost:9090", //webpack dev server running on 9090
+    // },
   }
 };
