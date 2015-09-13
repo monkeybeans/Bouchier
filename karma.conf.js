db299c28654618e7c8bd055fbbf0f50d471ca6b6
Herr Fisk <mac@noreply.com>
2015-09-13 17:31:09 +0200

    Polyfill for karma.

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      './node_modules/phantomjs-polyfill/bind-polyfill.js',
       'src/**/*.test.js',
     ],
 
