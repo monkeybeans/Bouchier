dc0ab18f41bd017125c40e501ed34e7024db3e5e
Herr Fisk <mac@noreply.com>
2015-11-22 22:25:28 +0100

    Removed navigation

--- src/start.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Navigation from './components/navigation';
-import './mixins/overview.scss';
-
-console.info('login verified...');
-React.render(
-    React.createElement(Navigation),
-    document.getElementById('navigation')
-);
