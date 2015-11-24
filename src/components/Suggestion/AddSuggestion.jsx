7a90a514ea91a34df567502a609553db4cd513fe
Herr Fisk <mac@noreply.com>
2015-11-24 21:46:28 +0100

    Refactoring suggestion

--- src/components/Suggestion/AddSuggestion.jsx
+++ src/components/Suggestion/AddSuggestion.jsx
@@ -33,28 +33,40 @@ export default class AddSuggestion extends React.Component {
 
     }
 
+    createList(listItems) {
+      const list = listItems.map((suggestion)=>{
+          return (
+            <div key={suggestion.id}>
+              <h2>{suggestion.name}</h2>
+              <p>{suggestion.description}</p>
+            </div>
+          );
+        }
+      );
+
+      return (<div className="suggestion-list">{list}</div>);
+    }
+
+    createAdder() {
+      let adder = (
+        <div className="suggestion-adder">
+          <a href="#" >+ add a suggestion</a>
+          <form id="new-suggestion" onSubmit={this.addSuggestion.bind(this)} ref={(ref) => this.newSuggestionForm = ref}>
+            <div><input type="input" name="newSuggestionName"></input></div>
+            <div><textarea type="textarea" name="newSuggestionSummary" rows="10" cols="50"></textarea></div>
+            <div><input type="submit" value="Submit suggestion"/></div>
+          </form>
+        </div>
+      );
+
+      return adder;
+    }
+
     render(){
         return(
             <div className="vote">
-                <div>
-                  { this.state.suggestions.map( (suggestion)=>{
-                      return (
-                        <div key={suggestion.id}>
-                          <h2>{suggestion.name}</h2>
-                          <p>{suggestion.description}</p>
-                        </div>
-                      );
-                    })
-                  }
-                </div>
-                <div>
-                  <a href="#" >+ add a suggestion</a>
-                  <form id="new-suggestion" onSubmit={this.addSuggestion.bind(this)} ref={(ref) => this.newSuggestionForm = ref}>
-                    <div><input type="input" name="newSuggestionName"></input></div>
-                    <div><textarea type="textarea" name="newSuggestionSummary" rows="10" cols="50"></textarea></div>
-                    <div><input type="submit" value="Submit suggestion"/></div>
-                  </form>
-                </div>
+              {this.createList(this.state.suggestions)}
+              {this.createAdder()}
             </div>
         );
     };
